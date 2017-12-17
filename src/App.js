92915eea57f26103ef81788df45a8ce9f8018afb
Noah Lipsyc <n.lipsyc@gmail.com>
2017-12-17 18:13:34 -0500

    Authentication form working.  Sotring the auth token in the auth form.  Next up is passing it to the mediclist so it can make its call

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import SysStatus from './components/SysStatus';
+import AuthenticationForm from './components/AuthenticationForm';
 
 class App extends Component {
   render() {
@@ -12,6 +13,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to Akira</h1>
         </header>
       <SysStatus />
+      <AuthenticationForm />
         <p className="App-intro">
         </p>
       </div>
--- src/components/AuthenticationForm.js
+++ src/components/AuthenticationForm.js
@@ -1,26 +1,65 @@
 
 import React, { Component } from 'react';
 
-class Authenticator extends Component {
-  constructor() {
-
+class AuthenticationForm extends Component {
+  constructor(props) {
+    super(props);
     this.state = {
       authToken: '',
+      userCreds: {
+        userEmail: '',
+        userPassword: '',
+      },
     };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  componentDidMount(){
+  handleSubmit(event) {
     fetch('https://sandbox.akira.md/api/auth', {
       method: 'post',
       body: JSON.stringify({
-        email: userEmail,
-        password: userPassword,
+        email: this.state.userCreds.userEmail,
+        password: this.state.userCreds.userPassword,
       }),
     }
+  )
+  .then(response => {
+      if (response.ok) {
+        response.json().then((data) => {
+          const responseData = data;
+          this.setState({ authToken: responseData.token });
+        });
+      };
+    });
+    event.preventDefault();
+  }
+
+  handleChange = (fieldName) => (event) => {
+    const auth = this.state.userCreds;
+    const newAuth = {
+      ...auth,
+      [fieldName]: event.target.value,
+    };
+    this.setState({ userCreds: newAuth });
+  };
+
+  render() {
+    return (
+    <form onSubmit={this.handleSubmit}>
+      <label>
+        Email
+        <input type='text' onChange={this.handleChange('userEmail')} value={this.state.userCreds.userEmail}/>
+      </label>
+      <label>
+        Password
+        <input type='password' onChange={this.handleChange('userPassword')} value={this.state.userCreds.userPassword}/>
+      </label>
+      <input type='submit' value='Log in' />
+    </form>
   );
-}
-
-render(){
-
 };
 }
+
+export default AuthenticationForm;
